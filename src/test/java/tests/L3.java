b6f58dae53f8016d0811215a178023db0abdd534
Ivane <ivanvozny@gmail.com>
2020-08-09 19:50:49 +0300

    +alfabank test

--- src/test/java/tests/L3.java
+++ src/test/java/tests/L3.java
@@ -6,18 +6,20 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-@Tag("tinkoff_tests")
+@Tag("alfabank_test")
 public class L3 {
-
-    void tintkoffTest(){
+@Test
+    void alfabankTest(){
         Configuration. holdBrowserOpen = true;
-        open("https://tinkoff.ru");
-        $(byText("Вклады")).closest("a").click();
+        open("https://alfabank.ru");
+        $(byText("Вклады")).click();
+        $$(byText("Депозиты")).find(visible).click();
+        //$(byText("Архивные счета")).click();
 }
 
 
