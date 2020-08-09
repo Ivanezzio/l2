48ed9f2aab2517d87a54de818116434ff7a536ce
Ivane <ivanvozny@gmail.com>
2020-08-09 12:41:41 +0300

    upgrade test

---
+++ src/test/java/tests/PleerTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+@Tag("pleer_test")
+
+public class PleerTest {
+    @Test
+    void selenidePleerTests()    {
+        Configuration. holdBrowserOpen = true;
+//открыть
+        open("https://www.pleer.ru/");
+//поиск
+        $(byName("text")).setValue("шуруповерт").pressEnter();
+//выбор товара
+        $(byId("list_items")).shouldHave(text("makita")).click();
+//добавить в корзину
+
+
+
+    }
+
+
+
+
+}
